a8cabbb7bcc24a2e8fd34b3585ab8e984c85a5b1
Braighton <braighton.polack@gmail.com>
2020-11-05 21:41:11 -0800

    Redux Configuration and basic reducers
    Initial reducer and states configured for alert message component, along with basic Redux setup for global state management

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -3,25 +3,31 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'; // Global app styles
 
 // Import Required Components
-import Header from './Header/Header';
-import Footer from './Footer/Footer';
+import Header from './layout/Header/Header';
+import Footer from './layout/Footer/Footer';
 import Landing from './pages/Landing/Landing';
 import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
 
+// Redux
+import { Provider } from 'react-redux';
+import store from '../store';
+
 const App = () => {
 	return (
-		<Router>
-			<div className="App">
-				<Header />
-				<Switch>
-					<Route exact path="/" component={Landing} />
-					<Route exact path="/login" component={Login} />
-					<Route exact path="/register" component={Register} />
-				</Switch>
-				<Footer />
-			</div>
-		</Router>
+		<Provider store={store}>
+			<Router>
+				<div className="App">
+					<Header />
+					<Switch>
+						<Route exact path="/" component={Landing} />
+						<Route exact path="/login" component={Login} />
+						<Route exact path="/register" component={Register} />
+					</Switch>
+					<Footer />
+				</div>
+			</Router>
+		</Provider>
 	);
 }
 
